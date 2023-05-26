0e8db35d8ac208bd2391b7c20f3f310a7458b64a
Theosoni19 <tsonihekpon@gmail.com>
2023-05-26 19:58:52 +0200

    Freshly Restaurant

---
+++ instructions/js/index.js
@@ -0,0 +1,9 @@
+// Fichier Js personnalisé
+
+$(document).ready(function(){
+    // Initialisation de Slick Slider
+    $(".carousel-wrapper").slick();
+    
+    // Initialisation de Mixitup
+    let mixer = mixitup("#mixer-container");
+})
